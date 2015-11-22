8905a8405d366827a0aa7303bda309699f7cf1e8
kodama <javajava552000@yahoo.co.jp>
2015-11-22 12:17:00 +0900

    indexのview,controllerルーティングの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
   def show
     # itemというモデルに対してfindメソッドでidを検索させる
     @item = Item.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # 追加
+  get '/items' => 'items#index'
   get '/items/:id' => 'items#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
