3b4b5e326ad862313996a2e90baa4bd07df11705
kodama <javajava552000@yahoo.co.jp>
2015-11-22 12:25:27 +0900

    newの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
   def index
     @items = Item.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # 追加
-  get '/items' => 'items#index'
+  get '/items/new' => 'items#new'
   get '/items/:id' => 'items#show'
+  get '/items' => 'items#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
